5c93ec7f65a1028b73ece04261e99eb78b4c2889
inoovr <63936551+inoovr@users.noreply.github.com>
2020-04-19 12:59:52 -0300

    Add files via upload

--- viewer/build/vrview.js
+++ viewer/build/vrview.js
@@ -40007,51 +40007,6 @@ FusionPositionSensorVRDevice.prototype.getState = function() {
 };
 
 
-// variable declarations
-OverlayClicked: boolean = false;
-isIOS: boolean = false;
-
-ngOnInit () {
- this.isIOS = ((/iPad|iPhone|iPod/.test(navigator.userAgent)) && (typeof (DeviceMotionEvent as any).requestPermission === 'function'));
-}
-
-// for requesting permission on iOs 13 devices
-requestPermissionsIOS() {
- this.requestDeviceMotionIOS();
- this.requestDeviceOrientationIOS();
- this.overlayClicked = true;
-}
-
-// requesting device motion permission
-requestDeviceMotionIOS() {
- if (typeof (DeviceMotionEvent as any).requestPermission === 'function') {
-  (DeviceMotionEvent as any).requestPermission()
-   .then(permissionState => {
-   if (permissionState === 'granted') {
-    window.addEventListener('devicemotion', () => {});
-   }
-  })
-   .catch(console.error);
- } else {
-  // handle regular non iOS 13+ devices
- }
-}
-
-// requesting device orientation permission
-requestDeviceOrientationIOS() {
- if (typeof (DeviceOrientationEvent as any).requestPermission === 'function') {
-  (DeviceOrientationEvent as any).requestPermission()
-   .then(permissionState => {
-   if (permissionState === 'granted') {
-    window.addEventListener('deviceorientation', () => {});
-   }
-  })
-   .catch(console.error);
- } else {
-  // handle regular non iOS 13+ devices
- }
-}
-
 FusionPositionSensorVRDevice.prototype.getOrientation = function() {
   // Convert from filter space to the the same system used by the
   // deviceorientation event.
