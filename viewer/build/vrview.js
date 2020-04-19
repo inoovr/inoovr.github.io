a1d74482080e412fd5154c6729496e77b588239d
inoovr <63936551+inoovr@users.noreply.github.com>
2020-04-19 12:49:49 -0300

    Add files via upload

--- viewer/build/vrview.js
+++ viewer/build/vrview.js
@@ -40006,35 +40006,51 @@ FusionPositionSensorVRDevice.prototype.getState = function() {
   }
 };
 
-  function onClick() {
-    // feature detect
-    if (typeof DeviceMotionEvent.requestPermission === 'function') {
-      DeviceMotionEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('devicemotion', () => {});
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-    }
-  }
 
-  function onClick() {
-    // feature detect
-    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
-      DeviceOrientationEvent.requestPermission()
-        .then(permissionState => {
-          if (permissionState === 'granted') {
-            window.addEventListener('deviceorientation', () => {});
-          }
-        })
-        .catch(console.error);
-    } else {
-      // handle regular non iOS 13+ devices
-    }
-  }
+// variable declarations
+OverlayClicked: boolean = false;
+isIOS: boolean = false;
+
+ngOnInit () {
+ this.isIOS = ((/iPad|iPhone|iPod/.test(navigator.userAgent)) && (typeof (DeviceMotionEvent as any).requestPermission === 'function'));
+}
+
+// for requesting permission on iOs 13 devices
+requestPermissionsIOS() {
+ this.requestDeviceMotionIOS();
+ this.requestDeviceOrientationIOS();
+ this.overlayClicked = true;
+}
+
+// requesting device motion permission
+requestDeviceMotionIOS() {
+ if (typeof (DeviceMotionEvent as any).requestPermission === 'function') {
+  (DeviceMotionEvent as any).requestPermission()
+   .then(permissionState => {
+   if (permissionState === 'granted') {
+    window.addEventListener('devicemotion', () => {});
+   }
+  })
+   .catch(console.error);
+ } else {
+  // handle regular non iOS 13+ devices
+ }
+}
+
+// requesting device orientation permission
+requestDeviceOrientationIOS() {
+ if (typeof (DeviceOrientationEvent as any).requestPermission === 'function') {
+  (DeviceOrientationEvent as any).requestPermission()
+   .then(permissionState => {
+   if (permissionState === 'granted') {
+    window.addEventListener('deviceorientation', () => {});
+   }
+  })
+   .catch(console.error);
+ } else {
+  // handle regular non iOS 13+ devices
+ }
+}
 
 FusionPositionSensorVRDevice.prototype.getOrientation = function() {
   // Convert from filter space to the the same system used by the
